2b7aca2c84d9932b0253aa74fac3a326ce4d6849
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-03-14 20:04:30 +0700

    change sound to simple

--- render.R
+++ render.R
@@ -1,4 +1,7 @@
 
+# Run this to render document to muliple format at the same time,
+# YAML parameters are passed from the Rmd document
+
 # HTML
 rmarkdown::render("./single-doc.Rmd", "bookdown::html_document2")
 
@@ -6,4 +9,4 @@ Sys.sleep(1) # Avoid crash when run both simultaneously
 # PDF
 rmarkdown::render("./single-doc.Rmd", "bookdown::pdf_document2")
 
-beepr::beep(8) # Play sound when finished
+beepr::beep(1) # Play sound when finished
